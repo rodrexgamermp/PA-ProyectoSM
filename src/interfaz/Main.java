4b7d9fb8ed9d1ade86518e10d71bab1e71b94090
lgsember <lgsember@gmail.com>
2023-09-22 09:53:26 -0300

    CommitTest

---
+++ src/interfaz/Main.java
@@ -0,0 +1,13 @@
+package interfaz;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		JOptionPane.showMessageDialog(null, "Hello");
+
+	}
+
+}
