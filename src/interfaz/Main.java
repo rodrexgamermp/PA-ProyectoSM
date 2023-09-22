05eb65a30a08fb2fce88315aaeaab3a5cf070542
lgsember <lgsember@gmail.com>
2023-09-22 10:14:25 -0300

    ramaLuke

--- src/interfaz/Main.java
+++ src/interfaz/Main.java
@@ -7,6 +7,8 @@ public class Main {
 	public static void main(String[] args) {
 		
 		JOptionPane.showMessageDialog(null, "Hello");
+		
+		JOptionPane.showMessageDialog(null, "Cambios en la rama Luke");
 
 	}
 
